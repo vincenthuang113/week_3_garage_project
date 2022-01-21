288d5e756c9b175814abd895ab30a3f53a0b5d09
vincenthuang113 <vincenthuang113@gmail.com>
2022-01-21 11:28:16 -0500

    first edit

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,1 +1,19 @@
 # Start Your Code here
+
+class Garage:
+    def __init__(self):
+        self.tickets = [1:10]
+        self.parkingSpaces = [1:10]
+        self.currentTicket = {}
+    
+    def takeTicket(self):
+        ticket = int(input("Please take a ticket | 1,2,3,4,5,6,7,8,9,10 "))
+        self.currentTicket[ticket] = ""
+        self.tickets.remove(ticket)
+        print(f"You took ticket no.{ticket}! ")
+
+    def payForParking(self):
+        pass
+
+    def leavesGarage(self):
+        pass
